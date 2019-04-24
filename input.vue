671d7d932cc18fa0c5c11f54223395e36348c967
gorunoob <46263616+gorunoob@users.noreply.github.com>
2019-04-24 22:04:35 +0800

    Add files via upload

---
+++ input.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="form-group">
+    <h3 
+      v-if="!editable" 
+      @click="editable = true">
+      {{title}}
+    </h3>
+    <input 
+      v-else 
+      type="text" 
+      :value="title" 
+      @input="changeTitle" 
+      @keyup.enter="editable = false">
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      default: '默认标题',
+      type: String
+    }
+  },
+  data () {
+    return {
+      editable: false
+    }
+  },
+  methods: {
+    changeTitle (e) {
+      this.$emit('updata:title', e.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
